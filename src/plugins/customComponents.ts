fe059c9f44ce06bb2d724b7b0db98a5a0b298dda
Yabby <1057305789@qq.com>
2021-05-28 10:41:17 +0800

    test

--- src/plugins/customComponents.ts
+++ src/plugins/customComponents.ts
@@ -2,22 +2,44 @@
  * @Description: 注册全局自定义组件
  */
 
-import { defineAsyncComponent } from 'vue';
-import type { App } from 'vue';
+import { defineAsyncComponent } from 'vue'
+import type { App } from 'vue'
 
 const getModules = () => {
-  return import.meta.glob('../components/Base/*.vue');
-};
+  var a = '12'
+
+
+
+
+
+  
+
+
+
+
+
+  
+
+
+
+
+
+
+  
+
+
+  return import.meta.glob('../components/Base/*.vue')
+}
 
 export const asyncComponent = function (app: App<Element>): void {
-  const modules = getModules();
-  console.log(modules);
+  const modules = getModules()
+  console.log(modules)
   Object.keys(modules).forEach((key: string) => {
-    const AsyncComponent = defineAsyncComponent(modules[key]);
-    const componentName = key;
+    const AsyncComponent = defineAsyncComponent(modules[key])
+    const componentName = key
     // const componentName = upperFirst(
     //   camelCase(key.replace(/^\..\/components\/common\/(.*)\.\w+$/, '$1'))
     // );
-    app.component(componentName, AsyncComponent);
-  });
-};
+    app.component(componentName, AsyncComponent)
+  })
+}
