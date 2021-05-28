98b5f53665916f79d24f36fa75d934e4f5426cc0
Yabby <1057305789@qq.com>
2021-05-28 14:06:53 +0800

    feat: husky-lint

--- src/plugins/customComponents.ts
+++ src/plugins/customComponents.ts
@@ -6,9 +6,6 @@ import { defineAsyncComponent } from 'vue'
 import type { App } from 'vue'
 
 const getModules = () => {
-  var a = '1'
-  console.log(a)
-
   return import.meta.glob('../components/Base/*.vue')
 }
 
